0e1b53008c14c123b9e05d8cee5518691032d1a7
Cole Cianflone <colecianflone123@gmail.com>
2022-03-20 12:35:11 -0500

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,80 +1,84 @@
+// global variables used in logic below.
+var userChoice = "";
+var computerChoice = "";
+
+// Subscribing to startGame
+document.getElementById("start").addEventListener("click",startGame);
+
+// Subscribing all buttons to getWinner function. I do this to be able to compare which button was clicked.
+document.getElementById("btnRock").addEventListener("click", () => {
+  userChoice = 'rock';
+  computerChoice = getComputerChoice();
+  getWinner();
+});
+document.getElementById("btnPaper").addEventListener("click", () => {
+  userChoice = 'paper';
+  computerChoice = getComputerChoice();
+  getWinner();
+});
+document.getElementById("btnScissors").addEventListener("click", () => {
+  userChoice = 'scissors';
+  computerChoice = getComputerChoice();
+  getWinner();
+});
+
+
+/* Functions */
+// Compares strings to get winner.
+function getWinner()
+{
+  if (userChoice === computerChoice) {
+    window.alert("The game is a tie!");
+    return;
+  }
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper'){
+      window.alert("You won!");
+    } else {
+      window.alert("You won!");
+    } 
+  }
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      window.alert("The computer won :(");
+    } else {
+      window.alert("You won!");
+    }
+  }
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      window.alert("The computer won!");
+    } else {
+      window.alert("You won!");
+    }
+  }
+}
+
+// Returns string to compare in getWinner function.
+function getComputerChoice()
+{
+  let randomNumber = Math.floor(Math.random() * 3) + 1
+
+  switch (randomNumber) 
+  {
+    case 1:
+      return 'rock';
+    case 2:
+      return 'paper';
+    case 3:
+      return 'scissors';
+  }
+}
+
+// Hides start button and shows game buttons when clicked.
 function startGame()
 {
   var buttonsToShow = document.getElementsByClassName("gameButtons");
 
-  for(i = 0; i < buttonsToShow.length ; i++)
+  for(i = 0; i < buttonsToShow.length; i++)
   {
       buttonsToShow[i].style.display = "block";
   }
 
   document.getElementById("start").style.display = "none";
 }
-
-// Subscribing to event
-document.getElementById("start").addEventListener("click",startGame)
-
-
-
-function getUserChoice(userInput) 
-{
-  userInput = userInput.toLowerCase();
-
-  if (userInput === 'rock' || userInput === 'paper' || userInput ==='scissors' ) {
-    return userInput;
-  } else {
-    console.log('Error!');
-  }
-};
-
-function getComputerChoice()
-{
-  let randomNumber = Math.floor(Math.random() * 3);
-
-  switch (randomNumber) {
-  case 0:
-    return 'rock';
-  case 1:
-    return 'paper';
-  case 2:
-    return 'scissors'
-  }
-}
-
-function determineWinner(userChoice,computerChoice)
-{
-  if (userChoice === computerChoice) {
-    return 'The game is a tie!';
-  }
-  if (userChoice === 'rock') {
-    if (computerChoice === 'paper'){
-      return 'The user won!';
-    } else {
-    return 'You won!';
-    } 
-  }
-  if (userChoice === 'paper') {
-    if (computerChoice === 'scissors') {
-      return 'The computer won!';
-    } else {
-      return 'You won!';
-    }
-  }
-  if (userChoice === 'scissors') {
-    if (computerChoice === 'rock') {
-      return 'The computer won!';
-    } else {
-      return 'You won!';
-    }
-  }
-}
-
-function playGame()
-{
-  const userChoice = getUserChoice('scissors');
-  const computerChoice = getComputerChoice();
-  console.log('You threw: ' + userChoice);
-  console.log('The computer threw: ' + computerChoice);
-  console.log(determineWinner(userChoice, computerChoice));
-};
-
-playGame();
