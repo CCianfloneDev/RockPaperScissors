acfd4400a0c9cc571dfcf1c36fad933d66340bd8
Cole Cianflone <colecianflone123@gmail.com>
2022-03-27 15:38:58 -0500

    Update rockPaperScissors.js
    Boxes are now changing images, not properly but getting there.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,32 +4,69 @@ document.getElementById("start").addEventListener("click", startGame);
     document.getElementById(button).addEventListener("click", () => {
         let userChoice = button.substring("btn".length).toLowerCase();
         let computerChoice = getComputerChoice();
-        getWinner(userChoice, computerChoice);
+        getWinner(computerChoice, userChoice);
     });
 });
 
 function getWinner(playerOne, playerTwo) {
+
+
     if (playerOne == playerTwo) return window.alert("The game is a tie!");
     switch (playerOne) {
         case "rock":
-            if (playerTwo == "paper") return window.alert("The computer won :(");
+            if (playerTwo == "paper")
+            {
+                let userChoiceImage = document.getElementById("userChoiceImage");
+                userChoiceImage.setAttribute('src', 'images/Paper.png')
+
+                let computerChoiceImage = document.getElementById("computerChoiceImage");
+                computerChoiceImage.setAttribute('src', 'images/Rock.png');
+
+                return window.alert("The computer won :(");
+            } 
             break;
         case "paper":
-            if (playerTwo == "scissors") return window.alert("The computer won :(");
+            if (playerTwo == "scissors") 
+            {
+                let userChoiceImage = document.getElementById("userChoiceImage");
+                userChoiceImage.setAttribute('src', 'images/Scissors.png')
+
+                let computerChoiceImage = document.getElementById("computerChoiceImage");
+                computerChoiceImage.setAttribute('src', 'images/Paper.png');
+
+                return window.alert("The computer won :(");
+            }
             break;
         case "scissors":
-            if (playerTwo == "rock") return window.alert("The computer won :(");
+            if (playerTwo == "rock") 
+            {
+                let userChoiceImage = document.getElementById("userChoiceImage");
+                userChoiceImage.setAttribute('src', 'images/Rock.png')
+
+                let computerChoiceImage = document.getElementById("computerChoiceImage");
+                computerChoiceImage.setAttribute('src', 'images/Scissors.png');
+
+                return window.alert("The computer won :(");
+            }
             break;
-    } window.alert("You won!");
+        default:
+            window.alert("You won!");
+            break;
+    }
 }
 
 function getComputerChoice() {
-	return ["rock", "paper", "scissors"] [Math.floor(Math.random() * 3)]
+    return ["rock", "paper", "scissors"] [Math.floor(Math.random() * 3)]
 }
 
 function startGame() {
-	Array.from(document.getElementsByClassName("gameButtons")).forEach((button) => {
-		button.style.display = "block";
-	})
+    Array.from(document.getElementsByClassName("gameButtons")).forEach((button) => {
+        button.style.display = "block";
+    })
+
+    // When start button is clicked  the initial image goes away.
+    let startingImage = document.getElementById("initialImage");
+    startingImage.setAttribute('src', '');
+
     document.getElementById("start").style.display = "none";
 }
